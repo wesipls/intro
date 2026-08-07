55958f10a7dcb8f0bc84ab30f943cb515b6fe5db
Kim Valkama <hello@valkama.kim>
2026-08-07 10:26:32 +0300

    essential performance improvements

--- js/vim.js
+++ js/vim.js
@@ -1,4 +1,4 @@
 console.log(
-    `%cHope you like Vim; if we work together, that's going to be your new default server editor.`,
-    "color: #4af626; font-family: monospace; font-size: 13px; line-height: 1.5;"
+    `%c/base/64`,
+    "color: #4af626; font-family: monospace; font-size: 13px; line-height: 1.5;",
 );
